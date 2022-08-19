edfdff12096f4f7ca7b89f5eded8e22baf76abf2
shamin daniil <danya.shamin.06@mail.ru>
2022-08-19 17:25:43 +0300

    add two Java files

---
+++ ProgramImport.java
@@ -0,0 +1,8 @@
+public class ProgramImport{
+    String stroka;
+    Integer chislo;
+    ProgramImport(String stroka, Integer chislo){
+        this.stroka = stroka;
+        this.chislo = chislo;
+    }
+}
---
+++ ProgramMain.java
@@ -0,0 +1,6 @@
+public class ProgramMain {
+    public static void main(String[] args){
+        ProgramImport a = new ProgramImport("Name", 11);
+        System.out.println(a.stroka+" "+a.chislo);
+    }
+}

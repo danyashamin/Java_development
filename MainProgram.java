adbaa031b0b81f519250de9a0a4c56302f38b88a
shamin daniil <danya.shamin.06@mail.ru>
2022-08-29 16:15:06 +0300

    corrected mistake

---
+++ MainProgram.java
@@ -0,0 +1,7 @@
+public class MainProgram{
+    public static void main(String[] args){
+        Integer a = 2;
+        Integer b = 3;
+        System.out.println(a&b);
+    }
+}
--- programMain.java
+++
@@ -1,7 +0,0 @@
-public class programMain{
-    public static void main(String[] args){
-        byte one = 4;
-        byte one_2 = 5;
-        System.out.println(one&one_2);
-    }
-}

132e2fbe87c38e22323b0e52e4470b1aa773de34
shamin daniil <danya.shamin.06@mail.ru>
2022-09-02 15:11:17 +0300

    did bitwise multiplication

--- MainProgram.java
+++ MainProgram.java
@@ -1,7 +1,5 @@
 public class MainProgram{
     public static void main(String[] args){
-        Integer a = 2;
-        Integer b = 3;
-        System.out.println(a&b);
+        System.out.println(" ");
     }
 }

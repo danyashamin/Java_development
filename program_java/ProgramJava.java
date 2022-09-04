915e9e2a71f024ed4b831eeae7a40205f026181c
shamin daniil <danya.shamin.06@mail.ru>
2022-09-04 22:35:59 +0300

    delete all and add Scanner examplar

---
+++ program_java/ProgramJava.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ProgramJava {
+    public static void main(String[] args){
+        System.out.println("Hello!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input your text:");
+        String in_string = in.next();
+        System.out.println("Finish");
+    }
+}

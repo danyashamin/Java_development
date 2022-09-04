babd15a913589a844b11ccda9a05301099f1c2ab
shamin daniil <danya.shamin.06@mail.ru>
2022-09-04 20:34:32 +0300

    train to import lwjgl_lib

--- ProgramJava.java
+++ ProgramJava.java
@@ -1,9 +1,9 @@
 import lwjgl_lib.JARS.*;
 import lwjgl_lib.NATIVES.*;
-import lwjgl_lib.SOURCES.*;;
+import lwjgl_lib.SOURCES.*;
 
 public class ProgramJava{
     public static void main(String[] args){
-        System.out.println("OK!");
+        System.out.println("Import lwjgl_lib");
     }
 }

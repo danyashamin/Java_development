198ab97ea2fa038c06d633fefe50824ba0542dad
shamin daniil <danya.shamin.06@mail.ru>
2022-09-05 18:13:03 +0300

    add one condition (boolean) constant

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,9 +1,11 @@
+import java.io.*;
 import java.util.zip.*;
 
 public class JavaProgram{
     public static void main(String[] args){
-        byte a = 9;
-        byte b = 5;
-        System.out.println(a^b);
+        Integer a = 2;
+        Integer b = 3;
+        boolean condition = a == b;
+        System.out.println(condition);
     }
 }

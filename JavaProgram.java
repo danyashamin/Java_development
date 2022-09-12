bf51664c753bb8327911d7e5d8ee6094b709e92d
shamin daniil <danya.shamin.06@mail.ru>
2022-09-12 17:24:15 +0300

    add JavaExpection

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class JavaProgram{
     public static void main(String[] args){
-        Integer zero = 0;
+        Scanner inputInt = new Scanner(System.in);
+        System.out.print("Input num:");
+        Integer Int_my = inputInt.nextInt();
         try{
             Integer notZero = 1;
-            double resule = notZero/zero;
+            double result = notZero/Int_my;
         } catch(ArithmeticException curExpection){
             System.out.println("На ноль то делить нельзя!");
         }

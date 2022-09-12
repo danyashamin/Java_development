267eaf9e5970ae08cd067f435d6ed98d86953eb6
shamin daniil <danya.shamin.06@mail.ru>
2022-09-12 14:20:39 +0300

    add Java Expection

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,18 +1,11 @@
-import java.util.Scanner;
-
 public class JavaProgram{
     public static void main(String[] args){
-        function();
-    }
-    public static void function(){
-        Integer constant = 4;
-        for (int i = 0; i<5; i++){
-            Scanner in = new Scanner(System.in);
-            Integer inInteger = in.nextInt();
-            System.out.println(inInteger.equals(constant));
-            if (inInteger.equals(constant)){
-                break;
-            }
+        Integer zero = 0;
+        try{
+            Integer notZero = 1;
+            double resule = notZero/zero;
+        } catch(ArithmeticException curExpection){
+            System.out.println("На ноль то делить нельзя!");
         }
     }
 }

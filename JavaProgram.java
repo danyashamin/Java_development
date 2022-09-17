e28425b078048f25300679fb2ffd6cdbdba5bd71
shamin daniil <danya.shamin.06@mail.ru>
2022-09-17 13:31:27 +0300

    add char symbol

--- JavaProgram.java
+++ JavaProgram.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class JavaProgram{
     public static void main(String[] args){
-        Scanner inputInt = new Scanner(System.in);
-        System.out.print("Input num:");
-        Integer Int_my = inputInt.nextInt();
-        try{
-            Integer notZero = 1;
-            double result = notZero/Int_my;
-        } catch(ArithmeticException curExpection){
-            System.out.println("На ноль то делить нельзя!");
-        }
+        char a = 65;
+        System.out.println(a);
     }
 }
